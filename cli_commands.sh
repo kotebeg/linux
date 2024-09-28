3f362785a8ba24dba58fb35ed588d5e7cea4b10a
kotebeg <kotebeg@gmail.com>
2024-09-28 19:27:28 +0400

    added venv commands

---
+++ cli_commands.sh
@@ -0,0 +1,7 @@
+# install vm-tools for copy paste
+sudo apt get upsate
+sudo apt-get install open-vm-tools-desktop
+
+# create virtual env on specific python versionb
+python3.12 -m venv myenv
+source myenv/bin/activate
