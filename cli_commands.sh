19f49e9e504731a779f681642d377de4da868251
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-11 17:20:50 +0400

    Update cli_commands.sh
    correct commands, adding visual studio code installation

--- cli_commands.sh
+++ cli_commands.sh
@@ -1,7 +1,10 @@
 # install vm-tools for copy paste
-sudo apt get upsate
+sudo apt-get update
 sudo apt-get install open-vm-tools-desktop
 
 # create virtual env on specific python versionb
 python3.12 -m venv myenv
 source myenv/bin/activate
+
+# install visual studio code   "code_1.94.2-1728494015_amd64.deb" is file name, it can be other, 
+sudo dpkg -i code_1.94.2-1728494015_amd64.deb
