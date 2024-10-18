f719da37a6e3ecd867a6a0736ab832692d1cbf2b
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-19 02:20:37 +0400

    Update cli_commands.sh
    x 11 check connected monitors

--- cli_commands.sh
+++ cli_commands.sh
@@ -14,5 +14,23 @@ source myenv/bin/activate
 sudo dpkg -i code_1.94.2-1728494015_amd64.deb
 
 
-# print display protocol
+# print display protocol X11 or wayland
 echo $XDG_SESSION_TYPE
+
+
+# X11 detect coneccted monitors
+xrandr
+
+# Screen 0: minimum 8 x 8, current 1920 x 1080, maximum 16384 x 16384
+# HDMI-1 connected primary 1920x1080+0+0 (normal left inverted right x axis y axis) 477mm x 268mm
+#    1920x1080     60.00*+  50.00    59.94  
+#    1680x1050     59.88  
+#    1600x900      60.00  
+#    1280x1024     60.02  
+#    1280x720      60.00    50.00    59.94  
+#    1024x768      60.00  
+#    800x600       60.32    56.25  
+#    720x576       50.00  
+#    720x480       60.00    59.94  
+#    640x480       60.00    59.94  
+# DP-1 disconnected (normal left inverted right x axis y axis)
