6a653b67b1e21933ea94e8bad8790810eca9b274
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-19 16:45:31 +0400

    Update virtualization.sh
    adding instllation  Vmware worksrtation

--- virtualization.sh
+++ virtualization.sh
@@ -1,2 +1,6 @@
-# How to uninstall VMware Player on ubuntu?
+# uninstall VMware Player on ubuntu?
 sudo vmware-installer -u vmware-player
+
+# dowonload  <filename>.bundle
+# install Vwmare Workstation  sudo sh <filename>.bundle
+sudo sh VMware-Workstation-Full-17.6.1-24319023.x86_64.bundle
