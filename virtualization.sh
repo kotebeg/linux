8c9d8145bcd1daf1da3a2b003ae32431e0ddf5c3
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-19 16:52:28 +0400

    Update virtualization.sh
    vm tools for copy paste options

--- virtualization.sh
+++ virtualization.sh
@@ -4,3 +4,8 @@ sudo vmware-installer -u vmware-player
 # dowonload  <filename>.bundle
 # install Vwmare Workstation  sudo sh <filename>.bundle
 sudo sh VMware-Workstation-Full-17.6.1-24319023.x86_64.bundle
+
+
+# install vm-tools for copy paste
+sudo apt-get update
+sudo apt-get install open-vm-tools-desktop
