4bae25a77de4b4604a55cecc7619c064533a3cf8
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-21 20:42:26 +0400

    Update fedora_cli.sh
    adding htop installation

--- fedora_cli.sh
+++ fedora_cli.sh
@@ -1,5 +1,13 @@
-# install nano on fedora
+# -------------------install nano on fedora
 # Update the package list:
 sudo dnf update -y
 # Install nano:
 sudo dnf install nano -y
+
+# -------------------install nano on htop
+# Update the package list:
+sudo dnf update -y
+# Install htop:
+sudo dnf install htop
+# Launch htop:
+htop
