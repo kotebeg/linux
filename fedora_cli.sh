58e3ea02e213f2031d262f9130e1388254b1a346
kotebeg <49250158+kotebeg@users.noreply.github.com>
2024-10-21 21:12:55 +0400

    Update fedora_cli.sh
    add terminator on fedora

--- fedora_cli.sh
+++ fedora_cli.sh
@@ -4,10 +4,16 @@ sudo dnf update -y
 # Install nano:
 sudo dnf install nano -y
 
-# -------------------install nano on htop
+# -------------------install nano on fedora
 # Update the package list:
 sudo dnf update -y
 # Install htop:
-sudo dnf install htop
+sudo dnf install htop -y
 # Launch htop:
 htop
+
+# -------------------install terminator on fedora
+sudo dnf update -y
+# Install htop:
+sudo dnf install terminator -y
+# Launch htop:
